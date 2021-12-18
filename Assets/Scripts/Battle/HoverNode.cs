439e31dd1ce67ddaac9bdbd7a6d82bdd17f49346
syc418 <47166720+syc418@users.noreply.github.com>
2021-12-17 16:58:49 -0800

    Sample Ink Trail & Sample Touch Node
    Under Forest_Zheng scene:
    Sample Ink Trail - Trail Renderer
    Sample Touch Node - On Mouse Over

---
+++ Assets/Scripts/Battle/HoverNode.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class HoverNode : MonoBehaviour
+{
+    //When the mouse hovers over the GameObject, it turns to this color (red)
+    public Color m_MouseOverColor = Color.red;
+
+    //This stores the GameObject’s original color
+    public Color m_OriginalColor;
+
+    //Get the GameObject’s mesh renderer to access the GameObject’s material and color
+    SpriteRenderer m_Renderer;
+
+    Tween tween_current;
+
+    void Start()
+    {
+        //Fetch the mesh renderer component from the GameObject
+        m_Renderer = GetComponent<SpriteRenderer>();
+        //Fetch the original color of the GameObject
+        m_OriginalColor = m_Renderer.color;
+    }
+
+    void OnMouseOver()
+    {
+        if (tween_current != null)
+        {
+            tween_current.Complete();
+        }
+        // Change the color of the GameObject to red when the mouse is over GameObject
+        m_Renderer.color = m_MouseOverColor;
+
+    }
+
+    void OnMouseExit()
+    {
+        // Reset the color of the GameObject back to normal
+        tween_current = m_Renderer.DOColor(m_OriginalColor, 2f);
+
+    }
+}
---
+++ Assets/Scripts/Battle/InkTrail.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InkTrail : MonoBehaviour
+{
+    private bool is_active = false;
+    public GameObject inkTrailPrefab;
+
+    public Camera camera_current;
+
+    public GameObject inkTrailParent;
+    public GameObject inkTrailCurrent;
+
+    private void Update()
+    {
+        if (is_active && inkTrailCurrent)
+        {
+            Vector3 position = camera_current.ScreenToWorldPoint(Input.mousePosition);
+            position.z = 0;
+            inkTrailCurrent.transform.position = position;
+        }
+
+        if (Input.GetMouseButtonDown(0) && !is_active) 
+        {
+            inkTrailCurrent = Instantiate<GameObject>(inkTrailPrefab);
+            inkTrailCurrent.transform.SetParent(inkTrailParent.transform);
+
+            Vector3 position = camera_current.ScreenToWorldPoint(Input.mousePosition);
+            position.z = 0;
+
+            inkTrailCurrent.transform.position = position;
+
+            is_active = true;
+        }
+
+        if (Input.GetMouseButtonUp(0) && is_active)
+        {
+            is_active = false;
+        }
+    }
+}

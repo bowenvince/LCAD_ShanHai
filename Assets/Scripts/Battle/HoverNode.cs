bdeb917a91b69e913ce70848126fee053588aabf
syc418 <47166720+syc418@users.noreply.github.com>
2021-12-20 15:46:56 -0800

    Updated Touch Node
    Added timer to touch node and failure color.

--- Assets/Scripts/Battle/HoverNode.cs
+++ Assets/Scripts/Battle/HoverNode.cs
@@ -1,44 +1,101 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 public class HoverNode : MonoBehaviour
 {
-    //When the mouse hovers over the GameObject, it turns to this color (red)
-    public Color m_MouseOverColor = Color.red;
+    //When the mouse hovers over the GameObject, it turns to this color (yellow)
+    public Color m_MouseOverColor;
 
     //This stores the GameObject’s original color
     public Color m_OriginalColor;
 
-    //Get the GameObject’s mesh renderer to access the GameObject’s material and color
-    SpriteRenderer m_Renderer;
+    //When the timer ran out without mouse over, it turns to this color (red)
+    public Color m_FailColor;
 
+    //total time to catch other wise fade out
+    public float fade_out_time;
+    private float fade_out_time_left;
+
+    //Get the GameObject’s Image Component (replaced mesh renderer) to access the GameObject’s material and color
+    Image m_Image;
+
+    //tempory store Tween obj
     Tween tween_current;
+
+
+    //track status
+    public bool is_complete;
+    public bool is_active;
+
+
 
     void Start()
     {
         //Fetch the mesh renderer component from the GameObject
-        m_Renderer = GetComponent<SpriteRenderer>();
+        m_Image = GetComponent<Image>();
         //Fetch the original color of the GameObject
-        m_OriginalColor = m_Renderer.color;
+        m_OriginalColor = m_Image.color;
+
+        fade_out_time_left = fade_out_time;
+
+        is_complete = false;
+
+        //is_active = false;
     }
 
     void OnMouseOver()
     {
-        if (tween_current != null)
+        if (!is_active)
         {
-            tween_current.Complete();
+            m_Image.color = m_FailColor;
         }
-        // Change the color of the GameObject to red when the mouse is over GameObject
-        m_Renderer.color = m_MouseOverColor;
+        else 
+        {
+            if (tween_current != null)
+            {
+                tween_current.Complete();
+            }
+            // Change the color of the GameObject to red when the mouse is over GameObject
+            m_Image.color = m_MouseOverColor;
+
+            is_complete = true;
+
+            m_Image.fillAmount = 1;
+        }
+        
 
     }
 
     void OnMouseExit()
     {
+        if (!is_active) return;
         // Reset the color of the GameObject back to normal
-        tween_current = m_Renderer.DOColor(m_OriginalColor, 2f);
+        tween_current = m_Image.DOColor(m_OriginalColor, 2f);
 
+    }
+
+    private void Update()
+    {
+        if (fade_out_time_left > 0)
+        {
+            fade_out_time_left -= Time.deltaTime;
+            if (!is_complete)
+            {
+                m_Image.fillAmount = Mathf.Clamp01(fade_out_time_left / fade_out_time);
+            }
+        }
+        else 
+        {
+            if (!is_complete)
+            {
+                m_Image.fillAmount = 1;
+                m_Image.color = m_FailColor;
+            }
+            
+        }
+        
     }
 }

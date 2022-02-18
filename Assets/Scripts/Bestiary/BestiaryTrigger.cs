aa561196be701a4d65e184a5681ec2b1781f6732
syc418 <47166720+syc418@users.noreply.github.com>
2022-02-17 21:02:54 -0800

    Item Notification
    Added item notification (fade in/out, sprite, text)

--- Assets/Scripts/Bestiary/BestiaryTrigger.cs
+++ Assets/Scripts/Bestiary/BestiaryTrigger.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class BestiaryTrigger : MonoBehaviour, ITrigger
 {
@@ -9,9 +10,17 @@ public class BestiaryTrigger : MonoBehaviour, ITrigger
     public int bestiary_element_index;
     public UnityEvent events;
 
+    public Sprite item_image;
+    public string item_name;
+
     public void AddBestiaryLine() 
     {
         BestiarySystem._this.UpdateBestiary(bestiary_set_index, bestiary_element_index, 1);
+    }
+
+    public void ShowNotification() 
+    {
+        ItemNotificationScript._this.show(item_image, item_name);
     }
 
     public void OnCall()
---
+++ Assets/Scripts/ItemNotificationScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ItemNotificationScript : Singleton<ItemNotificationScript>
+{
+    private CanvasGroup canvasGroup;
+    [SerializeField]
+    private TextMeshProUGUI text;
+    [SerializeField]
+    private string text_begin;
+    [SerializeField]
+    private Image image;
+
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        reset();
+    }
+
+    public void reset()
+    {
+        canvasGroup.alpha = 0;
+    }
+
+    public void show(Sprite sprite, string name) 
+    {
+        reset();
+        canvasGroup.DOComplete();
+        canvasGroup.DOFade(1, 0.5f);
+        text.text = text_begin + name;
+        image.sprite = sprite;
+        Sequence tween = DOTween.Sequence();
+        tween.AppendInterval(2f);
+        tween.Append(canvasGroup.DOFade(0, 1f));
+    }
+}

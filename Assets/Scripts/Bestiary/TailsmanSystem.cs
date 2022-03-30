049cbfda2ce78be24343387cf803c2ae9fc0ede8
syc418 <47166720+syc418@users.noreply.github.com>
2022-03-30 15:37:49 -0700

    Tailsman Update
    Tailsman Update with new assets, add animation.

---
+++ Assets/Scripts/Bestiary/TailsmanSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TailsmanSystem : MonoBehaviour
+{
+    #region Helper Class
+    [System.Serializable]
+    public class Orb
+    {
+        public string name;
+        public GameObject obj;
+    }
+    #endregion
+
+    private int current_index;
+
+    [SerializeField]
+    private List<Orb> orb_obj;
+
+    //switch orb from current to new index
+    public void SwitchOrb(int index)
+    {
+        orb_obj[current_index].obj.SetActive(false);
+        current_index = index;
+        orb_obj[current_index].obj.SetActive(true);
+    }
+
+    //check if current element match
+    public bool CheckElement(string name) 
+    {
+        return (name == orb_obj[current_index].name);
+    }
+}
